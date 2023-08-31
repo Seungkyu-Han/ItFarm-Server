25aa1dddb142496c075d89912ed905ab6c77d04b
Seungkyu-Han <>
2023-09-01 00:21:08 +0900

    [feat] start SpringBoot

---
+++ src/test/java/CoBo/ItFarm/ItFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package CoBo.ItFarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
