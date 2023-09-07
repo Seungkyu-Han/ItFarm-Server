e9e721808c6d5b11db38dc9e82b49e582fcea8ce
Seungkyu-Han <>
2023-09-07 10:02:52 +0900

    [feat] enum 초기 설정

--- src/main/java/CoBo/ItFarm/Data/Enum/WarningCategoryEnum.java
+++ src/main/java/CoBo/ItFarm/Data/Enum/WarningCategoryEnum.java
@@ -1,6 +1,17 @@
 package CoBo.ItFarm.Data.Enum;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum WarningCategoryEnum {
 
-    WATER_LEVEL,
+    DUMMY(false, ""),
+    DUMMY2(false, ""),
+    TOO_HOT(true, "");
+
+    private final boolean sendMail;
+
+    private final String message;
 }
