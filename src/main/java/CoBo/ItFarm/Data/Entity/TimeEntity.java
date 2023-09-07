8b8b38f82cb0f4e8a8b16e549cd1fc86c2bb6dd0
Seungkyu-Han <>
2023-09-07 09:59:00 +0900

    [refactor] Entity 정보 수정

--- src/main/java/CoBo/ItFarm/Data/Entity/TimeEntity.java
+++ src/main/java/CoBo/ItFarm/Data/Entity/TimeEntity.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
 
 @Entity(name = "time")
 @Data
+@NoArgsConstructor
 public class TimeEntity implements Serializable {
 
     @Id
@@ -18,4 +20,8 @@ public class TimeEntity implements Serializable {
     private Long id;
 
     private Timestamp time;
+
+    public TimeEntity(Timestamp time){
+        this.time = time;
+    }
 }
