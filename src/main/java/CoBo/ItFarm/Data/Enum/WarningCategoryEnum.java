1dfaa15a2c2d49acc266f6179956e9999d702a81
Seungkyu-Han <>
2023-09-07 22:37:37 +0900

    [feat] warning enum 추가

--- src/main/java/CoBo/ItFarm/Data/Enum/WarningCategoryEnum.java
+++ src/main/java/CoBo/ItFarm/Data/Enum/WarningCategoryEnum.java
@@ -7,11 +7,21 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum WarningCategoryEnum {
 
-    DUMMY(false, ""),
-    DUMMY2(false, ""),
-    TOO_HOT(true, "");
-
+    WATER_LEVEL_TOO_HIGH(false),
+    WATER_LEVEL_TOO_HIGH_OVER(true),
+    WATER_LEVEL_TOO_LOW(false),
+    FIELD_TOO_HOT_ON(false),
+    FIELD_TOO_HOT_OFF(false),
+    FIELD_TOO_HOT_OVER(true),
+    FIELD_TOO_COLD_ON(false),
+    FIELD_TOO_COLD_OFF(false),
+    FIELD_TOO_COD_OVER(true),
+    PH_TOO_HIGH(true),
+    PH_TOO_LOW(true),
+    EC_TOO_HIGH(true),
+    EC_TOO_LOW(true),
+    WATER_TOO_HOT(true),
+    WATER_TOO_COLD(true)
+    ;
     private final boolean sendMail;
-
-    private final String message;
 }
