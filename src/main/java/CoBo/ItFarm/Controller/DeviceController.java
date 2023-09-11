4ff72eb78346a541d93e726d30056e3f5e45c7b4
Seungkyu-Han <>
2023-09-11 23:21:56 +0900

    [feat] Entity 수정

--- src/main/java/CoBo/ItFarm/Controller/DeviceController.java
+++ src/main/java/CoBo/ItFarm/Controller/DeviceController.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
