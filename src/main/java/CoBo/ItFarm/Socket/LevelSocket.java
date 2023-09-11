9b9299afbbab10360bd282e10cacce8a9c9d36f8
Seungkyu-Han <>
2023-09-11 21:23:18 +0900

    [feat] socket 연결 작성

---
+++ src/main/java/CoBo/ItFarm/Socket/LevelSocket.java
@@ -0,0 +1,49 @@
+package CoBo.ItFarm.Socket;
+
+import CoBo.ItFarm.Data.Dto.Device.Res.DeviceLevelRes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LevelSocket extends TextWebSocketHandler {
+
+    private final List<WebSocketSession> webSocketSessionList = new ArrayList<>();
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        webSocketSessionList.add(session);
+        log.info("클라이언트 접속 : {}", session);
+    }
+
+    @EventListener
+    public void sendData(DeviceLevelRes deviceLevelRes){
+        for(WebSocketSession webSocketSession : webSocketSessionList){
+            try{
+                webSocketSession.sendMessage(new TextMessage(objectMapper.writeValueAsString(deviceLevelRes)));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        webSocketSessionList.remove(session);
+        log.info("클라이언트 접속 해제: {}", session);
+    }
+}
